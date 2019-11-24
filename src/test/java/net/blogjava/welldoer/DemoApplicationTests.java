31f957f803084570ad0610bfa39811570eeea460
welldoer <welldoer@263.net>
2019-11-24 16:13:21 +0800

    :fire: 回退版本/e6db699/：以 H2 数据库完成一版原书【2.1 使用 MySQL】章节内容

--- src/test/java/net/blogjava/welldoer/DemoApplicationTests.java
+++ src/test/java/net/blogjava/welldoer/DemoApplicationTests.java
@@ -1,12 +1,9 @@
 package net.blogjava.welldoer;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 class DemoApplicationTests {
 
 	@Test
