d275e7ee4ee5a2e21b9da9b525676120d6a034b5
welldoer <welldoer@263.net>
2019-11-29 14:35:26 +0800

    :white_check_mark: 增加 redis 发布/订阅的测试验证

---
+++ src/main/java/net/blogjava/welldoer/RedisSubListenerConfig.java
@@ -0,0 +1,34 @@
+package net.blogjava.welldoer;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.MessageListener;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
+
+@Configuration
+public class RedisSubListenerConfig {
+    @Bean
+    MessageListenerAdapter messageListener() {
+        //abstract methods overwrite
+        return new MessageListenerAdapter((MessageListener) (message, pattern) -> {
+            System.out.println("Message received: " + message.toString());
+        });
+    }
+
+    @Bean
+    RedisMessageListenerContainer redisContainer(RedisConnectionFactory connectionFactory) {
+        RedisMessageListenerContainer container
+                = new RedisMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        container.addMessageListener(messageListener(), topic());
+        return container;
+    }
+
+    @Bean
+    ChannelTopic topic() {
+        return new ChannelTopic("messageQueue");
+    }
+}
---
+++ src/test/java/net/blogjava/welldoer/RedisSubPubTest.java
@@ -0,0 +1,31 @@
+package net.blogjava.welldoer;
+
+import org.junit.ClassRule;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.testcontainers.containers.GenericContainer;
+
+@SpringBootTest
+public class RedisSubPubTest {
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+	@ClassRule
+	private static final GenericContainer redis = new GenericContainer<>("redis:5.0.3-alpine").withExposedPorts(6379);
+	
+	@BeforeAll
+	public static void init() {
+		redis.start();
+		
+		System.setProperty("spring.redis.host", redis.getContainerIpAddress());
+		System.setProperty("spring.redis.port", redis.getFirstMappedPort().toString());
+	}
+    
+    @Test
+    public void test(){
+        stringRedisTemplate.convertAndSend("messageQueue", "hello world");
+    }
+}
