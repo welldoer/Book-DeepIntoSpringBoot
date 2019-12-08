a0ed1bd86306e22fbc016067a2698c1082a03f7f
welldoer <welldoer@263.net>
2019-12-08 22:50:14 +0800

    :rotating_light: 按照 SonarCloud 建议微调

--- src/main/java/net/blogjava/welldoer/controller/MovieController.java
+++ src/main/java/net/blogjava/welldoer/controller/MovieController.java
@@ -15,10 +15,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.ui.ModelMap;
 import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -40,7 +41,7 @@ public class MovieController {
 	@Autowired
 	private Neo4jPagesService<MovieNeo4j> pagesService;
 
-	@RequestMapping("/new")
+	@GetMapping("/new")
 	public ModelAndView create(ModelMap model) {
 		String[] files = {"/images/movie/西游记.jpg", "/images/movie/西游记续集.jpg"};
 		model.addAttribute("files", files);
@@ -48,15 +49,15 @@ public class MovieController {
 		return new ModelAndView("movie/new");
 	}
 	
-	@RequestMapping(value = "/save", method = RequestMethod.POST)
-	public String save(@RequestBody MovieNeo4j movie) throws Exception {
+	@PostMapping(value = "/save")
+	public String save(@RequestBody MovieNeo4j movie) {
 		movieRepository.save(movie);
 		logger.info("新增 -> ID = {}", movie.getId());
 		
 		return "1";
 	}
 	
-	@RequestMapping("/{id}")
+	@GetMapping("/{id}")
 	public ModelAndView show(ModelMap model, @PathVariable Long id) {
 		Optional<MovieNeo4j> movie = movieRepository.findById(id);
 		model.addAttribute("movie", movie);
@@ -64,7 +65,7 @@ public class MovieController {
 		return new ModelAndView("movie/show");
 	}
 	
-	@RequestMapping("/edit/{id}")
+	@GetMapping("/edit/{id}")
 	public ModelAndView update(ModelMap model, @PathVariable Long id) {
 		Optional<MovieNeo4j> movie = movieRepository.findById(id);
 		String[] files = {"/images/movie/西游记.jpg", "/images/movie/西游记续集.jpg"};
@@ -79,8 +80,8 @@ public class MovieController {
 		return new ModelAndView("movie/edit");
 	}
 	
-	@RequestMapping(value = "/update", method = RequestMethod.POST)
-	public String update(MovieNeo4j movie, HttpServletRequest request) throws Exception {
+	@PostMapping(value = "/update")
+	public String update(MovieNeo4j movie, HttpServletRequest request) {
 		String rolename = request.getParameter("rolename");
 		String actorid = request.getParameter("actorid");
 		
@@ -93,7 +94,9 @@ public class MovieController {
 			
 			if (!StringUtils.isEmpty(rolename) && !StringUtils.isEmpty(actorid)) {
 				Optional<ActorNeo4j> actor = actorRepository.findById(Long.getLong(actorid));
-				orig.addRole(actor.get(), rolename);
+				if (actor.isPresent()) {
+					orig.addRole(actor.get(), rolename);
+				}
 			}
 			movieRepository.save(orig);
 			logger.info("修改 -> ID: {}", orig.getId());
@@ -102,16 +105,16 @@ public class MovieController {
 		return "1";
 	}
 	
-	@RequestMapping("/delete/{id}")
-	public String delete(@PathVariable Long id) throws Exception {
+	@GetMapping("/delete/{id}")
+	public String delete(@PathVariable Long id) {
 		movieRepository.deleteById(id);
 		logger.info("删除 -> ID: {}", id);
 		
 		return "1";
 	}
 	
-	@RequestMapping("/list")
-	public Page<MovieNeo4j> list(HttpServletRequest request) throws Exception {
+	@GetMapping("/list")
+	public Page<MovieNeo4j> list(HttpServletRequest request) {
 		String name = request.getParameter("name");
 		String page = request.getParameter("page");
 		String size = request.getParameter("size");
